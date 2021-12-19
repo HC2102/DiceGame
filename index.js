c55bc6df7db8203d30c884eecc9be745bc892546
HC2102 <78536932+HC2102@users.noreply.github.com>
2021-12-19 08:39:41 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var  randomNumber1 = Math.round(Math.random()*5+1);
+var  randomNumber2 = Math.round(Math.random()*5+1);
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+if(randomNumber1 < randomNumber2)
+{
+  document.querySelector("h1").innerText = "Player 2 wins! "+"🚩";
+}
+else if (randomNumber1>randomNumber2) {
+  document.querySelector("h1").innerText = "🚩"+" Player 1 wins!";
+}
+else{
+  document.querySelector("h1").innerText ="🚩" +" Draw "+"🚩";
+}
